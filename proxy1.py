0b743ad689ee1442783624983a11081bcf5ed60c
NIKZ3 <ngawade911@gmail.com>
2021-03-31 20:34:42 +0530

    Ip and Host Blacklisting

--- proxy1.py
+++ proxy1.py
@@ -10,6 +10,20 @@ class server:
         self.proxySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.proxySocket.bind(('', self.port))
         self.proxySocket.listen(10)
+        self.blackListUrls = dict()
+        self.blackListUsers = dict()
+
+        blackListUrlFile = open('blackListUrlFile.txt', 'r')
+        data = blackListUrlFile.readlines()
+
+        blackListUsers = open('blacklistUsers.txt', 'r')
+        blackListUsersData = blackListUsers.readlines()
+
+        for d in blackListUsersData:
+            self.blackListUsers[d] = 1
+
+        for d in data:
+            self.blackListUrls[d] = 1
 
     def initialiseServer(self):
 
@@ -58,8 +72,23 @@ class server:
         return details
 
     def serveRequest(self, clientSocket, clientAddr):
+        '''Creating connection to the client'''
         flag = 0
+
+        # userDataStore can be used to udata data in database
+        userDataStore = dict()
+        print(clientAddr)
+        userDataStore["UserIP"] = clientAddr[0]
         while 1:
+
+            # Checking if user is blackListed
+            if clientAddr[0] in self.blackListUsers:
+                updateToDatabase = True
+                print("User is blocked will be reported")
+                userDataStore["blackListUserAccess"] = 1
+                userDataStore["blackListUserIP"] = clientAddr[0]
+                break
+
             print("----------Waiting for Request----")
             try:
                 clientSocket.settimeout(5)
@@ -72,31 +101,57 @@ class server:
             request = request.decode()
             if(len(request) == 0):
                 break
-            # print(request)
 
-            x = "Referer"
+            httpRefererField = "Referer"
+            updateToDatabase = httpRefererField in request
+
             print("-----Modified Request--------")
             request = request.replace("Proxy-Connection:", "Connection:")
             print(request)
             details = self.parseRequest(request)
+
+            if details["webserver"] in self.blackListUrls:
+                userDataStore["blackListUrlAccess"] = 1
+                userDataStore["blackListUrl"] = details["webserver"]
+                print("IP not allowed you will be reported")
+                break
+
+            userDataStore["blackListAccess"] = 0
+            userDataStore["Url"] = details["webserver"]
+            '''Creating connection to the webServer'''
             if flag == 0:
                 webServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                webServer.connect((details["webserver"],   details["port"]))
-                flag = 1
+                webServer.settimeout(10)
+                try:
+                    webServer.connect(
+                        (details["webserver"],   details["port"]))
+                    flag = 1
+                except socket.timeout as err:
+                    print("-----Server Connection Failed------")
+                    break
             webServer.sendall(request.encode())
 
             print("--------waiting for Response----")
             data = webServer.recv(4096)
-            # print(data)
             while len(data):
-                # receive data from web server
-                clientSocket.send(data)  # send to browser/client
+                clientSocket.send(data)
                 data = webServer.recv(4096)
 
-        clientSocket.close()
-        webServer.close()
+        try:
+            clientSocket.close()
+            webServer.close()
+        except:
+            print(
+                "Connection to webServer was not opened or there was an error while closing the connection")
         #reply_end = "\r\n\r\n"
         # clientSocket.send(reply_end.encode())
+
+        if updateToDatabase == True:
+            self.addToDatabase(userDataStore)
+
+    #! TODO: Implementaion
+    def addToDatabase(self, userDataStore):
+        print("-----Adding data to database------")
 
 
 s = server(8080)
